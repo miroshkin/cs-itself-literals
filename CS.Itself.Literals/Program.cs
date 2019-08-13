1d0120331cde7df484c00fb86ec07760c18d6313
Anton Miroshkin <anton.miroshkin@gmail.com>
2019-08-13 16:53:10 +0300

    Added examples

--- CS.Itself.Literals/Program.cs
+++ CS.Itself.Literals/Program.cs
@@ -6,7 +6,25 @@ namespace CS.Itself.Literals
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.White;
+            
+            Console.WriteLine("\a\a\a"); //beep beep beep
+
+            string a = "asdf";
+            string b = "asdf";
+
+            Console.WriteLine("string a equals b : " + a == b + "\n\v");
+
+            
+            object c = "asdf";
+            object d = "asdf";
+
+            Console.WriteLine("object c equals d : " + a == b + "\n\v");
+
+            string e = @"Excellent work!";
+            Console.Write($"\'{e}\'");
         }
     }
 }
